e97e4c483de99b2c645e861424f270757e660712
maubard <marion.digiacomo@gmail.com>
2016-01-30 16:21:26 +0100

    Start My Areas

--- app/src/androidTest/java/com/marion/whitemadder/ApplicationTest.java
+++ app/src/androidTest/java/com/marion/whitemadder/ApplicationTest.java
@@ -4,8 +4,6 @@ import android.app.Application;
 import android.test.ApplicationTestCase;
 import android.util.Log;
 
-import com.marion.cadenceur.model.Area;
-
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
